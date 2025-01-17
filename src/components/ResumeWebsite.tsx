061c237114f01245199629ae341f1144fd8dcbdf
Okysu <yby@ecanse.com>
2025-01-17 12:15:24 +0800

    修复简历网站组件中的学校信息显示格式

--- src/components/ResumeWebsite.tsx
+++ src/components/ResumeWebsite.tsx
@@ -64,7 +64,7 @@ const ResumeWebsite: React.FC<ResumeProps> = ({
                 </a>
                 <div className="flex items-center">
                   <School className="w-5 h-5 mr-2" />
-                  {basicInfo.school}
+                  {basicInfo.school} 
                 </div>
               </div>
               <p className={`text-lg ${darkMode ? 'text-gray-300' : 'text-gray-600'} mb-6`}>
